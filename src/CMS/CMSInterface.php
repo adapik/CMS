f03ccdcebf394465548afc0b477f965db6aed85d
Nurlan Mukhanov <102@mp.kz>
2020-07-13 11:54:34 +0600

    wrong copyright

--- src/CMS/CMSInterface.php
+++ src/CMS/CMSInterface.php
@@ -2,13 +2,10 @@
 /**
  * CMSInterface
  *
- * @package      Adapik\CMS
- * @copyright    Copyright © Real Time Engineering, LLP - All Rights Reserved
- * @license      Proprietary and confidential
- * Unauthorized copying or using of this file, via any medium is strictly prohibited.
- * Content can not be copied and/or distributed without the express permission of Real Time Engineering, LLP
- *
- * @author       Written by Nurlan Mukhanov <nmukhanov@mp.kz>, июль 2020
+ * @author    Nurlan Mukhanov <nurike@gmail.com>
+ * @copyright 2020 Nurlan Mukhanov
+ * @license   https://en.wikipedia.org/wiki/MIT_License MIT License
+ * @link      https://github.com/Adapik/CMS
  */
 
 namespace Adapik\CMS;
